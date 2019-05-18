d700476b689979d56f8cf9f4024366b027367208
ITAM-Wonbin Kim <cross.wkim@gmail.com>
2019-05-18 20:21:12 +0900

    ...

--- Web/web.js
+++ Web/web.js
@@ -3,6 +3,9 @@ const app = express();
 const bodyParser = require('body-parser');
 const session = require('express-session');
 
+var FCM = require('fcm-node');
+var fcm = new FCM("AAAAxMHSmYI:APA91bGCGU3KWor7047u7cUWOn8dcBo1FwTzmgRVCeW9dlG4HbM1biXmzD193gYxisPJBKSC9FmXXe7l2Pzy4_6LgQdyfcoZS-vKc5plaO5IEAGFnsYxz0NCVVYAdflRETYDPpJX-IX2");
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
@@ -29,5 +32,31 @@ app.get('/orders', (req, res) => {
 });
 
 app.get('/completed', (req, res) => {
-	orders[req.query.token]
+	var data = {
+		to: orders[req.query.token],
+		notification: {
+			title: "SmartBell",
+			body: "메뉴가 준비되었습니다.",
+			sound: "default",
+		},
+		priority: "high",
+		restricted_package_name: "team10.smartbell",
+		data: {
+		}
+	};
+
+	fcm.send(data, function(err, response) {
+		delete orders[req.query.token]
+
+		if (err) {
+			console.error('메시지 발송에 실패했습니다.');
+			console.error(err);
+			res.status(200).send(err);
+			return;
+		}
+	 
+		console.log('메시지가 발송되었습니다.');
+		console.log(response);
+		res.status(200).send(true);
+	});
 })
